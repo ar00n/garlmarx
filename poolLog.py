9947229a2fc1d345f5f601f8c90826a6bd3d217e
ar0n <asimpole.as@gmail.com>
2018-03-09 17:36:27 +0000

    Nearly there!

--- poolLog.py
+++ poolLog.py
@@ -1,10 +1,19 @@
 import json
 import urllib.request
 from time import sleep
+from ast import literal_eval
 
 #Delay is the logging interval (in seconds)
 delay = 10
+count = 0
 log = {}
+log['count'] = '0'
+
+try:
+    f = open('hashes.log', 'r')
+    log = literal_eval(f.read())
+except:
+    print('No log found.')
 
 while True:
     url = 'http://garlmarx.fun/api/stats'
@@ -12,19 +21,24 @@ while True:
     r = urllib.request.urlopen(req).read()
     json2 = json.loads(r.decode('utf-8'))
     miners = json2["pools"]["garlicoin"]["workers"]
+    log['count'] = str(int(log['count']) + 1)
 
     for item in miners:
         try:
             old = log[item]
+            uptime = log[item + 'uptime']
         except:
             old = 0
+            uptime = 0
         if "MH" in miners[item]["hashrateString"]:
             hashRate = float(miners[item]["hashrateString"][:-3]) * 1000
             log[item] = (old + hashRate) / 2
+            log[item + 'uptime'] = str(int(uptime) + 1)
         elif "KH" in miners[item]["hashrateString"]:
             hashRate = float(miners[item]["hashrateString"][:-3])
             log[item] = (old + hashRate) / 2
-
+            log[item + 'uptime'] = str(int(uptime) + 1)
+            
     file = open('hashes.log', 'w')
     file.write(str(log))
     file.close()
--- poolPayout.py
+++ poolPayout.py
@@ -2,27 +2,48 @@ from subprocess import call
 from subprocess import check_output
 import json
 
-#Minimum hashrate for payouts
-minHash = 100
+merged = ""
+count = 0
 
-#Gets wallet balance.
-balance = check_output(["garlicoin-cli", "getbalance"])
-balance = balance.decode('utf-8')
-#balance = 50
+#Minimum hashrate in kH/s and minimum uptime 0-1
+minHash = 1
+minUptime = 0.5
+
+#Gets wallet balance and takes away 0.1 to avoid fee issues.
+#balance = check_output(["garlicoin-cli", "getbalance"])
+#balance = balance.decode('utf-8')
+balance = 50
+balance -= 0.1
 
 #Gets miner info from log.
 with open('hashes.log','r') as inf:
     miners = eval(inf.read())
 
+for item in miners:
+    if 'uptime' in item:
+        None
+    elif 'count' in item:
+        None
+    else:
+        count += 1
+
 #Calculates how much each miner gets.
-payout = str(float(balance) / len(miners))
-merged = ""
+payout = str(float(balance) / count)
 
 for item in miners:
-    if miners[item] > minHash:
-        merged = merged + '\\"' + item + '\\":' + payout + ','
+    if 'uptime' in item:
+        None
+    elif 'count' in item:
+        None
+    else:
+        miner = (float(miners[item]) / float(miners['count'])) * float(miners[item + 'uptime'])
+        uptime = float(miners[item + 'uptime']) / float(miners['count'])
+        if (miner > minHash and uptime > minUptime):
+            print(uptime)
+            merged = merged + '\\"' + item + '\\":' + payout + ','
 
 merged = merged[:-1]
 command = """sendmany "" "{""" + merged + '}"'
+print(command)
 
-call(["garlicoin-cli", command])
+#call(["garlicoin-cli", command])
